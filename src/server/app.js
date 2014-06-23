16944eaa0ddce61b817ceecbae4840b69dc02010
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-22 18:28:17 -0700

    update server app

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,19 @@
-var express = require('express');
-var app = module.exports = express();
+var express = require('express'),
+app = express(),
+logger = require('morgan'),
+bodyParser = require('body-parser'),
+cookieSession = require('cookie-session'),
+everyauth = require('everyauth');
+require('./auth/github.js')(everyauth);
 
+app.use(logger());
+app.use(cookieSession({
+  keys: ['secret1', 'secret2'],
+  secureProxy: true
+}));
+app.use(bodyParser.json());
+app.use(everyauth.middleware());
 app.use(express.static(__dirname + '/../../public'));
+var router = require('./router');
+app.use(router);
+module.exports = app;
