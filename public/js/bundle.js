90d3e2560bcd891e571a20c29b58ea08f73dac9a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-23 14:22:40 -0700

    board_id

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9,23 +9,24 @@ module.exports = app;
 },{"./controllers/board":2,"./controllers/nav":3}],2:[function(require,module,exports){
 module.exports = function(app) {
   app.controller('BoardController', ['$http', function($http) {
+    this.id = '1';
     this.name = "Empty Board"; 
     this.columns = [];
     var board = this;
-    $http.get('/boards/1').success(function(data) {
+    $http.get('/boards/'+board.id).success(function(data) {
       if (data.board) {
         board.name = data.board.name;
         board.columns = data.board.columns;
       }
     });
     this.removeColumn = function(col) {
-      $http.delete('/boards/1/columns/'+col).success(function(data) {
+      $http.delete('/boards/'+board.id+'/columns/'+col).success(function(data) {
         if (data.board)
           board.columns = data.board.columns;
       });
     }
     this.removeCard = function(col, row) {
-      $http.delete('/boards/1/columns/'+col+'/cards/'+row).success(function(data) {
+      $http.delete('/boards/'+board.id+'/columns/'+col+'/cards/'+row).success(function(data) {
         if (data.board)
           board.columns = data.board.columns;
       });
--- src/client/controllers/board.js
+++ src/client/controllers/board.js
@@ -1,22 +1,23 @@
 module.exports = function(app) {
   app.controller('BoardController', ['$http', function($http) {
+    this.id = '1';
     this.name = "Empty Board"; 
     this.columns = [];
     var board = this;
-    $http.get('/boards/1').success(function(data) {
+    $http.get('/boards/'+board.id).success(function(data) {
       if (data.board) {
         board.name = data.board.name;
         board.columns = data.board.columns;
       }
     });
     this.removeColumn = function(col) {
-      $http.delete('/boards/1/columns/'+col).success(function(data) {
+      $http.delete('/boards/'+board.id+'/columns/'+col).success(function(data) {
         if (data.board)
           board.columns = data.board.columns;
       });
     }
     this.removeCard = function(col, row) {
-      $http.delete('/boards/1/columns/'+col+'/cards/'+row).success(function(data) {
+      $http.delete('/boards/'+board.id+'/columns/'+col+'/cards/'+row).success(function(data) {
         if (data.board)
           board.columns = data.board.columns;
       });
