e3aede31233613c7e8308123519e8fdc960ed549
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-22 22:03:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var dotenv = require('dotenv');
 dotenv.load();
 var app = require(__dirname+'/src/server/app.js');
 var port = process.env.PORT || 3000;
-app.listen(3000);
+app.listen(port);
 console.log("Listening on http://0.0.0.0:"+port);
