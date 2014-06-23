a513b291237de6920c17e29848a5ae9ec3b6ca34
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-22 18:27:32 -0700

    add session controller

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,13 @@
-var app =  angular.module('app', []);
+var app = angular.module('app', []);
+
+app.controller('SessionController', ['$http', function($http) {
+  this.loggedIn = false;
+  var session = this;
+  $http.get('/session.json').success(function(data) {
+    console.log(data);
+    session.loggedIn = data.auth && data.auth.loggedIn;
+  });
+}]);
 
 app.controller('BoardController', function() {
   this.name = "MyBoard"; 
