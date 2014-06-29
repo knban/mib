f879c519f8753d21a81a5e220271347e2663a2b4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-29 15:21:09 -0700

    drop backbone

--- src/server/user.js
+++ src/server/user.js
@@ -1,16 +1,10 @@
-module.exports = require('backbone').Model.extend({
-  initialize: function () {
+function User(){
     this.auth = this.get('auth');
     this.loggedIn = this.auth.loggedIn;
     if (this.loggedIn) {
-      this.identifier = this.computeIdentifier();
+      var provider = Object.keys(this.auth)[0];
+      var id = this.auth[provider].user.id;
+      var login = this.auth[provider].user.login;
+      this.identifier = provider+":"+login;
     }
-  },
-
-  computeIdentifier: function () {
-    var provider = Object.keys(this.auth)[0];
-    var id = this.auth[provider].user.id;
-    var login = this.auth[provider].user.login;
-    return provider+":"+login;
-  }
-})
+}
