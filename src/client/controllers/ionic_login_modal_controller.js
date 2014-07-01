a0fc6de262fa8a04454cda44bf4072edd7ddbf6c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-01 03:53:55 -0700

    export ionic login controller

---
+++ src/client/controllers/ionic_login_modal_controller.js
@@ -0,0 +1,26 @@
+module.exports = ['$scope', '$ionicModal', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('views/login_modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+}];
