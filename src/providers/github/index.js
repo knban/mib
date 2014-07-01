a87793c2a041c51ed5e154e0557cace6967e005b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-01 04:37:42 -0700

    setup github scoped html template

--- src/providers/github/index.js
+++ src/providers/github/index.js
@@ -4,7 +4,6 @@ var info = {
   iconUrl: "/images/github_48px.png"
 };
 
-
 module.exports.info = info;
 module.exports.cardHandler = require('./card_handler')(info);
 module.exports.cardProvider = require('./card_provider')(info);
