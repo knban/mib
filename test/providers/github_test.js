6773d1d92ac7f52f1389d7de878ed3bc35af1074
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-06 18:28:25 -0700

    update cardProvider tests

--- test/providers/github_test.js
+++ test/providers/github_test.js
@@ -1,4 +1,5 @@
 var helper = require('../test_helper'),
+sinon = helper.sinon,
 expect = helper.expect;
 
 var BoardController = helper.require('client/controllers/board_controller');
@@ -9,64 +10,80 @@ var li = require('li');
 var Endpoint = require('../../src/client/endpoint');
 
 describe("GitHub Provider", function() {
-  var app = {};
-  var api = new Endpoint();
-  api.setRoot('https://example.com/');
-  var tokens = {
-    github: "ghtoken"
-  };
-  var localStorage = tokens;
-  var board = null;
-  var provider = null;
-  var $http = null;
+  var board = null,
+  linker = null,
+  provider = null,
+  $api = null,
+  $github = null,
+  root = null,
+  github = null,
+  api = null,
+  request = null;
 
   beforeEach(function() {
-    $http = helper.fake$http();
-    linker = new BoardController[BoardController.length-1]($http).projectLinker;
-    board = {
-      _id: "1"
-    };
+    root = "https://example.com/";
+    board = { _id: "1" };
+    linker = {};
+
+    $api = sinon.stub();
+    $api.returns({
+      success: sinon.stub().returns({
+        error: sinon.stub()
+      })
+    })
+
+    $github = sinon.stub();
+    $github.returns({
+      success: sinon.stub().returns({
+        error: sinon.stub()
+      })
+    })
+
+    api = new Endpoint();
+    api.setRoot(root);
+    api.setClient('angular', $api, {
+      headers: { 'X-Auth-Token': 'apptoken' }
+    });
+
+    github = new Endpoint();
+    github.setRoot('https://api.github.com/');
+    github.setClient('angular', $github, {
+      headers: { 'Authorization': 'token ghtoken' }
+    });
+
+    provider = Provider(board, api, github, linker);
   });
 
   describe("installWebhook", function() {
     var hook = null;
-    var origin = "https://example.com";
+    
     var repo = { id: 123, hooks_url: "hooks" }
     beforeEach(function() {
-      $http.stub('post', function(stub) {}).returns({
-        success: function () {
-          return {
-            error:  function () {
-              
-            }
-          }
-        },
-      });
-      global.window = { location: { origin: origin } };
-      provider = Provider(board, linker, tokens, $http);
       provider.installWebhook(repo, function () {})
-      hook = $http.post.getCall(0).args[1];
+      request = $github.getCall(0).args[0];
+      hook = $github.getCall(0).args[0].data;
+    });
+    it("sends the user token correctly", function() {
+      expect(request.headers['Authorization']).to.eq('token ghtoken');
     });
     it("adds a webhook correctly", function() {
-      expect($http.post.callCount).to.eq(1);
-      expect($http.post.getCall(0).args[0]).to.eq("hooks");
+      expect(request.url).to.eq('https://api.github.com/hooks');
       expect(hook.name).to.eq("web");
       expect(hook.active).to.eq(true);
       expect(hook.events).to.include("issues");
       expect(hook.events).to.include("issue_comment");
-      expect(hook.config.url).to.eq(origin+"/boards/"+board._id+"/github/123/webhook");
+      expect(hook.config.url).to.eq(root+"boards/"+board._id+"/github/123/webhook");
       expect(hook.config.content_type).to.eq("json");
     });
   });
 
   describe("getRepos without pagination", function() {
     beforeEach(function() {
-      $http.stub('get', function(stub) {
-        return stub.yields([
+      $github.returns({
+        success: sinon.stub().yields([
           { name: "repo1" }, { name: "repo2" }, { name: "repo3" }
-        ], 200, function linkHeaders() {return ''});
-      });
-      provider = Provider(board, linker, tokens, $http);
+        ], 200, function linkHeaders() { return '' })
+      })
       provider.getRepos('url');
     });
     it("populates linker._Repos with 3 repos", function() {
@@ -74,59 +91,81 @@ describe("GitHub Provider", function() {
     });
   });
 
-  describe("importRepoIssues", function() {
-    var stub = null;
+  describe("postIssues", function() {
+    beforeEach(function() {
+      provider.postIssues([ {my:'issue'} ], { meta: 'data' });
+    });
+    it("posts issues to the api correctly", function() {
+      expect($api.getCall(0).args[0]).to.deep.eq(
+        { method: 'POST',
+          url: 'https://example.com/boards/1/cards/github',
+          data: { metadata: { meta: 'data' }, openIssues: [ { "my": "issue" } ] },
+          headers: { 'X-Auth-Token': 'apptoken' } } )
+    });
+  });
 
+  describe("importRepoIssues", function() {
     describe("issues not paginated", function() {
       beforeEach(function(done) {
-        $http.stub('get', function(stub) {
-          return stub.yields([
+        $github.returns({
+          success: sinon.stub().yields([
             { _id: 222 }
-          ], 200, function linkHeaders() {return ''});
-        });
-        stub = $http.stub('post', function(stub) {
-          return stub.yields({}, 200);
-        });
+          ], 200, function linkHeaders() { return '' }).returns({
+            error: sinon.stub()
+          })
+        })
         board._id = 2;
-        linker._Col = 1;
-        provider = Provider(board, linker, tokens, $http);
-        provider.importRepoIssues({ id: 111, issues_url: "test" }, done);
+        provider.importRepoIssues({ id: 111, issues_url: "test" }, function () {
+          request = $api.getCall(0).args[0];
+          done();
+        });
       });
-      it("uses the correct URL", function() {
-        expect(stub.getCall(0).args[0]).to.eq("https://example.com/boards/2/cards/github");
+      it("posts the issues to the backend in a single call", function() {
+        expect($api.callCount).to.eq(1);
+      });
+      it("POSTs data to the correct URL", function() {
+        expect(request.url).to.eq("https://example.com/boards/2/cards/github");
+        expect(request.method).to.eq("POST");
+        expect(request.data).to.be.ok;
       });
       it("supplies an id field sufficient for uniqueness matching", function() {
-        expect(stub.getCall(0).args[1].openIssues[0]._id).to.eq(222);
+        expect(request.data.openIssues[0]._id).to.eq(222);
       });
     });
 
     describe("with paginated issues", function() {
       beforeEach(function(done) {
-        $http.stub('get', function(stub) {
-          return stub.yields([
-            { id: 222 }
+        $github.returns({
+          success: sinon.stub().yields([
+            { _id: 222 }
           ], 200, function linkHeaders() {
-            $http.restub('get', function(stub) {
-              return stub.yields([ { id: 333 } ], 200, function(){});
+            $github.returns({ 
+              success: sinon.stub().yields([
+                { id: 333 }
+              ], 200, function() {}).returns({
+                error: sinon.stub()
+              })
             });
             return li.stringify({
               next: 'whatever'
             });
-          });
-        });
-        stub = $http.stub('post', function(stub) {
-          return stub.yields({}, 200);
+          }).returns({
+            error: sinon.stub()
+          })
+        })
+        board._id = 2;
+        provider.importRepoIssues({ id: 111, issues_url: "test" }, function () {
+          request = $api.getCall(0).args[0];
+          done();
         });
         board.id = 2;
-        linker._Col = 1;
-        provider = Provider(board, linker, tokens, $http);
         provider.importRepoIssues({ id: 111, issues_url: "test" }, function () {
           done();
         });
       });
 
       it("posts the issues to the backend in multiple calls", function() {
-        expect(stub.callCount).to.eq(2);
+        expect($api.callCount).to.eq(2);
       });
     });
   });
