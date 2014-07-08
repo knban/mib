fbd6740b52f7f64006e69557e5a8daec35af36fd
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-07 17:07:52 -0700

    implement findByIdAndMutate promise for Column

--- src/server/models/column.js
+++ src/server/models/column.js
@@ -1,1 +1,25 @@
-module.exports = require('mongoose').model('Column', require('./../schemata/column'));
+var mongoose = require('mongoose');
+var columnSchema = require('./../schemata/column')
+
+var Promise = require('bluebird');
+
+columnSchema.statics.findByIdAndMutate = function(id, mutator) {
+  var Column = this;
+  return new Promise(function (resolve, reject) {
+    Column.findOne({ _id : id }).exec(function(err, column) {
+      if (err) throw err
+      else if (column) {
+        mutator(column);
+        column.save(function (err) {
+          if (err) {
+            throw err;
+          } else {
+            return resolve(column);
+          }
+        });
+      } else return reject(new Error("column not found"));
+    });
+  })
+};
+
+module.exports = mongoose.model('Column', columnSchema);
