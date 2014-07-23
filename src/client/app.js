cfa53bf435146639b6985eee6228dd2b9c16f9d6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-23 18:09:42 -0500

    cleanup appjs, derive api from origin

--- src/client/app.js
+++ src/client/app.js
@@ -1,14 +1,10 @@
 var Endpoint = require('./endpoint');
 window.api = new Endpoint();
-api.setRoot(AppConfig.endpoint);
+api.setRoot(window.location.origin+"/api/v1/");
 
-var requires = ['ui.select2', 'smart'];
-if (window.ionic) requires.push('ionic');
-
-window.app = angular.module('app', requires)
+window.app = angular.module('app', ['ui.select2', 'smart'])
 .controller('SessionController', require('./controllers/session_controller'))
 .controller('BoardController', require('./controllers/board_controller'))
-.controller('IonicLoginModalController', require('./controllers/ionic_login_modal_controller'))
 .directive('ngTooltip', require('./directives/ng_tooltip'))
 .directive('ngJsonreader', require('./directives/ng_jsonreader'))
 .directive('ngSortable', require('./directives/ng_sortable'))
