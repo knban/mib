408f9823addd345acf79c6db938a2c7c57d406d4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-23 18:03:32 -0700

    add same parent logic and option parsing

--- src/client/directives/ng_dragndrop.js
+++ src/client/directives/ng_dragndrop.js
@@ -1,4 +1,4 @@
-var dragEl = null;
+var dragEl = null
 
 function swapNodes(a, b) {
   var aparent= a.parentNode;
@@ -10,20 +10,41 @@ function swapNodes(a, b) {
 module.exports = ['$parse', function ($parse) {
   return {
     compile: function ($el, attr) {
+      var json = attr.ngDragndrop
+        , options = null;
+
       return function (scope, $el) {
-        $el.prop('draggable', true);
-        $el.on('dragstart', function () {
-          dragEl = $el;
-          this.style.opacity = '0.4';
-        });
-        $el.on('dragend', function () {
-          this.style.opacity = '1';
-        });
-        $el.on('dragenter', function () {
-          swapNodes(dragEl[0], $el[0]);
-          //console.log(dragEl.text());
-          //console.log($el.text());
-        });
+        if (json) {
+          options = $parse(json)(scope);
+        } else {
+          options = {
+            dropzone: false
+          }
+        }
+
+        if (options.dropzone) {
+          $el.on('dragenter', function () {
+            if (dragEl.parent().get(0).isSameNode($el.get(0))) {
+              console.log('samenode');
+            } else {
+              $el.append(dragEl)
+            }
+          });
+        } else {
+          $el.prop('draggable', true);
+          $el.on('dragstart', function () {
+            dragEl = $el;
+            this.style.opacity = '0.4';
+          });
+          $el.on('dragend', function () {
+            this.style.opacity = '1';
+          });
+          $el.on('dragenter', function () {
+            swapNodes(dragEl[0], $el[0]);
+            //console.log(dragEl.text());
+            //console.log($el.text());
+          });
+        }
       };
 
 
