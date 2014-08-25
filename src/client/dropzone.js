d5e84f5afc6b3115002e1083c5d526cb27db7b25
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-24 22:40:05 -0700

    optimize the number of swaps

--- src/client/dropzone.js
+++ src/client/dropzone.js
@@ -1,6 +1,4 @@
-var _ = {
-  remove: require('lodash.remove')
-}
+var _ = { isEqual: require('lodash.isequal') }
 
 function Dropzone(columnCtrl) {
   this.controller = columnCtrl
@@ -11,6 +9,7 @@ var drag = null;
 
 Dropzone.prototype = {
   start: function (e) {
+    drag = null;
     drag = {
       card: $(e.item).data('id'),
       start: {
@@ -21,7 +20,17 @@ Dropzone.prototype = {
   },
   swapped: function ($1, $2) {
     drag.swaps = drag.swaps || []
-    drag.swaps.push([$1.data('id'), $2.data('id')])
+    var swap = [$1.data('id'), $2.data('id')]
+    var lastIndex = drag.swaps.length-1;
+    var lastSwap = drag.swaps[lastIndex]
+    if (lastSwap) {
+      if (_.isEqual(swap, lastSwap)) {
+        // Cancels out, remove it and don't add this swap.
+        drag.swaps.splice(lastIndex, 1)
+        return false;
+      }
+    }
+    drag.swaps.push(swap)
   },
   removed: function ($el) {
     drag.swaps = null;
