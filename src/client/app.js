9d470359f85257fcaec7ef704cffe3c7056545a0
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-24 18:57:10 -0700

    use modularized directive

--- src/client/app.js
+++ src/client/app.js
@@ -2,12 +2,10 @@ var Endpoint = require('./endpoint');
 window.api = new Endpoint();
 api.setRoot(window.location.origin+"/api/v1/");
 
-
-
 window.app = angular.module('app', ['ui.select2', 'smart'])
 .controller('SessionController', require('./controllers/session_controller'))
 .controller('BoardController', require('./controllers/board_controller'))
 .controller('ColumnController', require('./controllers/column_controller'))
 .directive('ngTooltip', require('./directives/ng_tooltip'))
 .directive('ngJsonreader', require('./directives/ng_jsonreader'))
-.directive('ngDragndrop', require('./directives/ng_dragndrop'))
+.directive('ngDragndrop', require('ng-dragndrop'))
